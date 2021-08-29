069a31d506c01c35262eb8ba0941a60ed0410f18
alehn <natsevsky@gmail.com>
2021-08-29 17:34:54 +0200

    Code cleanup

--- contracts/base/strategies/StrategyBase.sol
+++ contracts/base/strategies/StrategyBase.sol
@@ -225,7 +225,6 @@ abstract contract StrategyBase is IStrategy, Controllable {
       IERC20(rt).safeApprove(forwarder, amount);
       // it will sell reward token to Target Token and distribute it to SmartVault and PS
       uint256 targetTokenEarned = IFeeRewardForwarder(forwarder).distribute(amount, rt, _smartVault);
-
       if (targetTokenEarned > 0) {
         IBookkeeper(IController(controller()).bookkeeper()).registerStrategyEarned(targetTokenEarned);
       }
