ceec52a77b782b8e5415fca9bf775fe385c8509d
alehn <natsevsky@gmail.com>
2021-10-11 01:28:50 +0200

    Pool with factory tests.

--- test/strategies/StrategyTestUtils.ts
+++ test/strategies/StrategyTestUtils.ts
@@ -5,7 +5,7 @@ import {CoreContractsWrapper} from "../CoreContractsWrapper";
 import {SignerWithAddress} from "@nomiclabs/hardhat-ethers/signers";
 import {
   Announcer,
-  Controller,
+  Controller, IERC20,
   IStrategy,
   PriceCalculator,
   RewardToken,
@@ -297,7 +297,6 @@ export class StrategyTestUtils {
     await controller.setPSNumeratorDenominator(numenator, denumenatior);
   }
 
-
   public static async calculateTotalToClaim(calculator: PriceCalculator, strategy: IStrategy, rewardToken: RewardToken) {
     const targetTokenPrice = +utils.formatUnits(await calculator.getPriceWithDefaultOutput(rewardToken.address));
     console.log('targetTokenPrice', targetTokenPrice);
@@ -315,5 +314,20 @@ export class StrategyTestUtils {
     }
     return totalToClaim
   }
+
+  public static async buyAndApproveTokens(tokens: string[], amounts: number[], singer: SignerWithAddress, contractToApprove:string): Promise<void> {
+  for (const token of tokens) {
+    const i = tokens.indexOf(token);
+    let tokenContract = await ethers.getContractAt("IERC20", token, singer) as IERC20;
+    let targetBalance = amounts[i];
+    let tokenBalance = await tokenContract.balanceOf(singer.address);
+    while (tokenBalance < BigNumber.from(amounts[i])){
+      console.log(`Target bal: ${targetBalance}, current balance: ${tokenBalance}`);
+      await UniswapUtils.buyToken(singer, MaticAddresses.SUSHI_ROUTER, token, utils.parseUnits(amounts[i].toString()));
+      tokenBalance = await tokenContract.balanceOf(singer.address);
+    }
+    await tokenContract.approve(contractToApprove, tokenBalance, {from: singer.address});
+  }
+}
 
 }
---
+++ test/strategies/matic/balancer/PoolFactoryDeployment.test.ts
@@ -0,0 +1,119 @@
+import {ethers} from 'hardhat';
+import {BigNumber, Contract, utils} from 'ethers';
+import {expect} from 'chai';
+import {SignerWithAddress} from '@nomiclabs/hardhat-ethers/dist/src/signer-with-address';
+import {IBasePool, IERC20, IVault, IWeightedPool, MockAssetManagedPool, MockRewardsAssetManager} from "../../../../typechain";
+import {BytesLike} from "@ethersproject/bytes";
+import {MaticAddresses} from "../../../MaticAddresses";
+import {bn, fp} from "./helpers/numbers";
+import {UniswapUtils} from "../../../UniswapUtils";
+import {Erc20Utils} from "../../../Erc20Utils";
+import {encodeJoin} from "./helpers/mockPool";
+import {MAX_UINT256} from "./helpers/constants";
+import {formatFixed, parseFixed} from '@ethersproject/bignumber';
+import {StrategyTestUtils} from "../../StrategyTestUtils";
+
+
+async function deployPoolWithFactory(vault: IVault, singer: SignerWithAddress): Promise<IBasePool> {
+  const NAME = 'Three-token Test Weighted Pool';
+  const SYMBOL = '60WMATIC-20BTC-20WETH';
+  const swapFeePercentage = 0.5e16; // 0.5%
+  const ZERO_ADDRESS = '0x0000000000000000000000000000000000000000';
+  const WEIGHTED_POOL_FACTORY = '0x8E9aa87E45e92bad84D5F8DD1bff34Fb92637dE9';
+
+  const factory = await ethers.getContractAt('IWeightedPoolFactory', WEIGHTED_POOL_FACTORY, singer);
+  const tokens = [MaticAddresses.WMATIC_TOKEN, MaticAddresses.WBTC_TOKEN, MaticAddresses.WETH_TOKEN];
+  const weights = [fp(0.6), fp(0.2), fp(0.2)];
+  const tx = await factory.create(NAME, SYMBOL, tokens, weights,
+    swapFeePercentage, ZERO_ADDRESS);
+  const receipt = await tx.wait();
+
+  // We need to get the new pool address out of the PoolCreated event
+  const events = receipt.events.filter((e: any) => e.event === 'PoolCreated');
+  const poolAddress = events[0].args.pool;
+
+  // We're going to need the PoolId later, so ask the contract for it
+  const pool = await ethers.getContractAt('IWeightedPool', poolAddress) as IWeightedPool;
+  console.log("Pool deployed");
+  return pool;
+}
+
+const setup = async () => {
+  const [signer] = (await ethers.getSigners());
+
+  // Connect to balancer vault
+  let vault = await ethers.getContractAt(
+    "IVault", "0xBA12222222228d8Ba445958a75a0704d566BF2C8") as IVault;
+
+  // Deploy Pool
+  const pool = await deployPoolWithFactory(vault, signer);
+  const poolId = await pool.getPoolId();
+
+  console.log('############## Preparations completed ##################');
+
+  return {
+    data: {
+      poolId,
+    },
+    contracts: {
+      pool,
+      vault,
+    },
+  };
+};
+
+
+describe('Balancer WeightedPool', function () {
+  let vault: IVault, pool: Contract;
+  let poolId: BytesLike;
+  let singer: SignerWithAddress, investor: SignerWithAddress, other: SignerWithAddress;
+
+  before('Get user to act', async () => {
+    [singer, investor, other] = await ethers.getSigners();
+  });
+
+  beforeEach('set up asset manager', async () => {
+    const {contracts, data} = await setup();
+    vault = contracts.vault;
+    pool = contracts.pool;
+    poolId = data.poolId;
+  });
+
+  describe('WeightedPool initialization', () => {
+    it('Happy path', async () => {
+      const poolTokens = (await vault.getPoolTokens(poolId)).tokens;
+      const initialBalances = [bn(20000000000), bn(1000000), bn(40000000)];
+      const JOIN_KIND_INIT = 0;
+
+      const initUserData =
+        ethers.utils.defaultAbiCoder.encode(['uint256', 'uint256[]'],
+          [JOIN_KIND_INIT, initialBalances]);
+
+      // swap tokens to invest
+      await StrategyTestUtils.buyAndApproveTokens(
+        [MaticAddresses.WMATIC_TOKEN, MaticAddresses.WBTC_TOKEN, MaticAddresses.WETH_TOKEN],
+        [100000000, 10000, 10000],
+        singer, vault.address);
+
+      await vault.joinPool(poolId, singer.address, singer.address, {
+        assets: poolTokens,
+        maxAmountsIn: poolTokens.map(() => MAX_UINT256),
+        fromInternalBalance: false,
+        userData: initUserData,
+      });
+
+      const {balances} = await vault.getPoolTokens(poolId);
+      expect(balances[0]).to.be.eq(bn(20000000000));
+      expect(balances[1]).to.be.eq(bn(1000000));
+      expect(balances[2]).to.be.eq(bn(40000000));
+
+      console.log(`The pool now holds:`);
+      poolTokens.forEach((token, i) => {
+        console.log(`  ${token}: ${formatFixed(balances[i], 18)}`);
+      });
+      let bpt = await pool.balanceOf(singer.address);
+      expect(bpt).to.be.gt(0);
+      console.log(`Pool initiator received ${formatFixed(bpt, 18)} (BPT) in return`);
+    });
+  });
+});
