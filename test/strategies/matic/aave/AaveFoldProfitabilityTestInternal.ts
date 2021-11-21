b157400cc9e0f788988ff3c4d68b437b542d3216
olegnat <natsevsky@gmail.com>
2021-11-21 11:58:50 +0100

    - skip test with internal logic

--- test/strategies/matic/aave/AaveFoldProfitabilityTestInternal.ts
+++ test/strategies/matic/aave/AaveFoldProfitabilityTestInternal.ts
@@ -140,109 +140,109 @@ describe.skip('Universal Aave Fold profitability tests', async () => {
         await TimeUtils.rollback(snapshotBefore);
       });
 
-      it("Folding profitability calculations", async () => {
-
-        const vaultForUser = vault.connect(user);
-        const rt0 = (await vaultForUser.rewardTokens())[0];
-        const userUnderlyingBalance = await TokenUtils.balanceOf(underlying, user.address);
-        const undDec = await TokenUtils.decimals(underlying);
-
-        const isFoldEnabled = usageAsCollateralEnabled !== "true";
-        await strategy.setFold(isFoldEnabled);
-        console.log("deposit", deposit);
-        await VaultUtils.deposit(user, vault, utils.parseUnits(deposit, undDec));
-
-        const atDecimals = await TokenUtils.decimals(aToken);
-        const dtDecimals = await TokenUtils.decimals(debtToken);
-        const rtDecimals = await TokenUtils.decimals(MaticAddresses.WMATIC_TOKEN);
-
-        const rewardBalanceBefore = await TokenUtils.balanceOf(core.psVault.address, user.address);
-        console.log("rewardBalanceBefore: ", rewardBalanceBefore.toString());
-
-        const vaultBalanceBefore = await TokenUtils.balanceOf(core.psVault.address, vault.address);
-        console.log("vaultBalanceBefore: ", vaultBalanceBefore.toString());
-
-        const underlyingBalanceBefore = +utils.formatUnits(await TokenUtils.balanceOf(aToken, strategy.address), atDecimals);
-        console.log("underlyingBalanceBefore: ", underlyingBalanceBefore.toString());
-
-        const debtBalanceBefore = +utils.formatUnits(await TokenUtils.balanceOf(debtToken, strategy.address), dtDecimals);
-        console.log("debtBalanceBefore: ", debtBalanceBefore.toString());
-
-        const maticBefore = +utils.formatUnits(await TokenUtils.balanceOf(MaticAddresses.WMATIC_TOKEN, strategy.address), rtDecimals);
-
-        console.log("MATIC before: ", maticBefore.toString());
-        const data = await strategy.totalRewardPrediction(investingPeriod);
-
-        const supplyRewards = +utils.formatUnits(data[0], atDecimals) * Number(underlyingBalanceBefore);
-        const borrowRewards = +utils.formatUnits(data[1], dtDecimals) * Number(debtBalanceBefore);
-        let supplyUnderlyingProfitPredicted = +utils.formatUnits(data[2], 18) * Number(underlyingBalanceBefore);
-        const debtUnderlyingCostPredicted = +utils.formatUnits(data[3], 18) * Number(debtBalanceBefore);
-
-        console.log("supplyRewards:", supplyRewards);
-        console.log("borrowRewards:", borrowRewards);
-        console.log("supplyUnderlyingProfitPredicted:", supplyUnderlyingProfitPredicted);
-        console.log("debtUnderlyingCostPredicted :", debtUnderlyingCostPredicted);
-        console.log("======================================");
-
-
-        const dataWeth = await strategy.totalRewardPredictionInWeth(investingPeriod);
-
-        const supplyRewardsWeth = +utils.formatUnits(dataWeth[0], rtDecimals);
-        const borrowRewardsWeth = +utils.formatUnits(dataWeth[1], rtDecimals);
-        const supplyUnderlyingProfitWeth = +utils.formatUnits(dataWeth[2], atDecimals);
-        const debtUnderlyingCostWeth = +utils.formatUnits(dataWeth[3], dtDecimals);
-        let foldingProfPerToken;
-        if (isFoldEnabled){
-          foldingProfPerToken = supplyRewardsWeth + borrowRewardsWeth + supplyUnderlyingProfitWeth - debtUnderlyingCostWeth;
-        }else {
-          foldingProfPerToken = supplyRewardsWeth + supplyUnderlyingProfitWeth;
-        }
-        console.log("supplyRewardsWeth:", supplyRewardsWeth, "borrowRewardsWeth:", borrowRewardsWeth);
-        console.log("supplyUnderlyingProfitWeth:", supplyUnderlyingProfitWeth, "debtUnderlyingCostWeth:", debtUnderlyingCostWeth);
-        console.log("======================================");
-        console.log("Total foldingProfPerToken WETH:", foldingProfPerToken);
-        // expect(foldingProfPerToken).is.greaterThan(0);
-
-        await TimeUtils.advanceBlocksOnTs(investingPeriod);
-        await strategy.claimRewardPublic();
-
-        const underlyingBalanceAfter = +utils.formatUnits(await TokenUtils.balanceOf(aToken, strategy.address), atDecimals);
-
-        console.log("underlyingBalanceAfter: ", underlyingBalanceAfter.toString());
-
-        const debtBalanceAfter = +utils.formatUnits(await TokenUtils.balanceOf(debtToken, strategy.address), dtDecimals);
-        console.log("debtBalanceAfter: ", debtBalanceAfter.toString());
-
-        const debtCost = debtBalanceAfter - debtBalanceBefore;
-        console.log("debtCost: ", debtCost.toString());
-        const rewardsEarned = +utils.formatUnits(await TokenUtils.balanceOf(MaticAddresses.WMATIC_TOKEN, strategy.address), rtDecimals);
-        console.log("MATIC earned: ", rewardsEarned.toString());
-
-        const underlyingEarned = underlyingBalanceAfter - underlyingBalanceBefore;
-        console.log("UNDERLYING earned from supply: ", underlyingEarned.toString());
-
-        const underlyingEarnedTotal = underlyingEarned - debtCost;
-        console.log("UNDERLYING earned Total: ", underlyingEarnedTotal);
-
-
-        let rewardProfitPrediction = supplyRewards;
-        if (isFoldEnabled){
-          rewardProfitPrediction += borrowRewards;
-        }
-        console.log("rewardProfitPrediction (MATIC): ", rewardProfitPrediction.toString());
-
-        expect(rewardsEarned).is.approximately(rewardProfitPrediction, rewardProfitPrediction * 0.001, "Prediction of rewards profit is inaccurate")
-
-        console.log("underlyingEarnedPredicted: ", supplyUnderlyingProfitPredicted.toString());
-
-        console.log("debtUnderlyingCostPredicted: ", debtUnderlyingCostPredicted.toString());
-
-        if (debtCost > 0) {
-          supplyUnderlyingProfitPredicted = supplyUnderlyingProfitPredicted - debtUnderlyingCostPredicted;
-        }
-        expect(supplyUnderlyingProfitPredicted).is.approximately(underlyingEarnedTotal, Math.abs(underlyingEarnedTotal) * 0.1, "Prediction of underlying profit is inaccurate");
-
-      });
+      // it("Folding profitability calculations", async () => {
+      //
+      //   const vaultForUser = vault.connect(user);
+      //   const rt0 = (await vaultForUser.rewardTokens())[0];
+      //   const userUnderlyingBalance = await TokenUtils.balanceOf(underlying, user.address);
+      //   const undDec = await TokenUtils.decimals(underlying);
+      //
+      //   const isFoldEnabled = usageAsCollateralEnabled !== "true";
+      //   await strategy.setFold(isFoldEnabled);
+      //   console.log("deposit", deposit);
+      //   await VaultUtils.deposit(user, vault, utils.parseUnits(deposit, undDec));
+      //
+      //   const atDecimals = await TokenUtils.decimals(aToken);
+      //   const dtDecimals = await TokenUtils.decimals(debtToken);
+      //   const rtDecimals = await TokenUtils.decimals(MaticAddresses.WMATIC_TOKEN);
+      //
+      //   const rewardBalanceBefore = await TokenUtils.balanceOf(core.psVault.address, user.address);
+      //   console.log("rewardBalanceBefore: ", rewardBalanceBefore.toString());
+      //
+      //   const vaultBalanceBefore = await TokenUtils.balanceOf(core.psVault.address, vault.address);
+      //   console.log("vaultBalanceBefore: ", vaultBalanceBefore.toString());
+      //
+      //   const underlyingBalanceBefore = +utils.formatUnits(await TokenUtils.balanceOf(aToken, strategy.address), atDecimals);
+      //   console.log("underlyingBalanceBefore: ", underlyingBalanceBefore.toString());
+      //
+      //   const debtBalanceBefore = +utils.formatUnits(await TokenUtils.balanceOf(debtToken, strategy.address), dtDecimals);
+      //   console.log("debtBalanceBefore: ", debtBalanceBefore.toString());
+      //
+      //   const maticBefore = +utils.formatUnits(await TokenUtils.balanceOf(MaticAddresses.WMATIC_TOKEN, strategy.address), rtDecimals);
+      //
+      //   console.log("MATIC before: ", maticBefore.toString());
+      //   const data = await strategy.totalRewardPrediction(investingPeriod);
+      //
+      //   const supplyRewards = +utils.formatUnits(data[0], atDecimals) * Number(underlyingBalanceBefore);
+      //   const borrowRewards = +utils.formatUnits(data[1], dtDecimals) * Number(debtBalanceBefore);
+      //   let supplyUnderlyingProfitPredicted = +utils.formatUnits(data[2], 18) * Number(underlyingBalanceBefore);
+      //   const debtUnderlyingCostPredicted = +utils.formatUnits(data[3], 18) * Number(debtBalanceBefore);
+      //
+      //   console.log("supplyRewards:", supplyRewards);
+      //   console.log("borrowRewards:", borrowRewards);
+      //   console.log("supplyUnderlyingProfitPredicted:", supplyUnderlyingProfitPredicted);
+      //   console.log("debtUnderlyingCostPredicted :", debtUnderlyingCostPredicted);
+      //   console.log("======================================");
+      //
+      //
+      //   const dataWeth = await strategy.totalRewardPredictionInWeth(investingPeriod);
+      //
+      //   const supplyRewardsWeth = +utils.formatUnits(dataWeth[0], rtDecimals);
+      //   const borrowRewardsWeth = +utils.formatUnits(dataWeth[1], rtDecimals);
+      //   const supplyUnderlyingProfitWeth = +utils.formatUnits(dataWeth[2], atDecimals);
+      //   const debtUnderlyingCostWeth = +utils.formatUnits(dataWeth[3], dtDecimals);
+      //   let foldingProfPerToken;
+      //   if (isFoldEnabled){
+      //     foldingProfPerToken = supplyRewardsWeth + borrowRewardsWeth + supplyUnderlyingProfitWeth - debtUnderlyingCostWeth;
+      //   }else {
+      //     foldingProfPerToken = supplyRewardsWeth + supplyUnderlyingProfitWeth;
+      //   }
+      //   console.log("supplyRewardsWeth:", supplyRewardsWeth, "borrowRewardsWeth:", borrowRewardsWeth);
+      //   console.log("supplyUnderlyingProfitWeth:", supplyUnderlyingProfitWeth, "debtUnderlyingCostWeth:", debtUnderlyingCostWeth);
+      //   console.log("======================================");
+      //   console.log("Total foldingProfPerToken WETH:", foldingProfPerToken);
+      //   // expect(foldingProfPerToken).is.greaterThan(0);
+      //
+      //   await TimeUtils.advanceBlocksOnTs(investingPeriod);
+      //   await strategy.claimRewardPublic();
+      //
+      //   const underlyingBalanceAfter = +utils.formatUnits(await TokenUtils.balanceOf(aToken, strategy.address), atDecimals);
+      //
+      //   console.log("underlyingBalanceAfter: ", underlyingBalanceAfter.toString());
+      //
+      //   const debtBalanceAfter = +utils.formatUnits(await TokenUtils.balanceOf(debtToken, strategy.address), dtDecimals);
+      //   console.log("debtBalanceAfter: ", debtBalanceAfter.toString());
+      //
+      //   const debtCost = debtBalanceAfter - debtBalanceBefore;
+      //   console.log("debtCost: ", debtCost.toString());
+      //   const rewardsEarned = +utils.formatUnits(await TokenUtils.balanceOf(MaticAddresses.WMATIC_TOKEN, strategy.address), rtDecimals);
+      //   console.log("MATIC earned: ", rewardsEarned.toString());
+      //
+      //   const underlyingEarned = underlyingBalanceAfter - underlyingBalanceBefore;
+      //   console.log("UNDERLYING earned from supply: ", underlyingEarned.toString());
+      //
+      //   const underlyingEarnedTotal = underlyingEarned - debtCost;
+      //   console.log("UNDERLYING earned Total: ", underlyingEarnedTotal);
+      //
+      //
+      //   let rewardProfitPrediction = supplyRewards;
+      //   if (isFoldEnabled){
+      //     rewardProfitPrediction += borrowRewards;
+      //   }
+      //   console.log("rewardProfitPrediction (MATIC): ", rewardProfitPrediction.toString());
+      //
+      //   expect(rewardsEarned).is.approximately(rewardProfitPrediction, rewardProfitPrediction * 0.001, "Prediction of rewards profit is inaccurate")
+      //
+      //   console.log("underlyingEarnedPredicted: ", supplyUnderlyingProfitPredicted.toString());
+      //
+      //   console.log("debtUnderlyingCostPredicted: ", debtUnderlyingCostPredicted.toString());
+      //
+      //   if (debtCost > 0) {
+      //     supplyUnderlyingProfitPredicted = supplyUnderlyingProfitPredicted - debtUnderlyingCostPredicted;
+      //   }
+      //   expect(supplyUnderlyingProfitPredicted).is.approximately(underlyingEarnedTotal, Math.abs(underlyingEarnedTotal) * 0.1, "Prediction of underlying profit is inaccurate");
+      //
+      // });
 
     });
 
