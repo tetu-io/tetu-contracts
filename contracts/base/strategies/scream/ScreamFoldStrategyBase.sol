05397e8a990c09a7a6df346467deed328c3131b0
olegnat <natsevsky@gmail.com>
2021-12-02 01:57:13 +0100

    - main functions adoption

--- contracts/base/strategies/scream/ScreamFoldStrategyBase.sol
+++ contracts/base/strategies/scream/ScreamFoldStrategyBase.sol
@@ -18,9 +18,7 @@ import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "../FoldingBase.sol";
 import "../../../third_party/iron/CompleteRToken.sol";
 import "../../../third_party/scream/IScreamController.sol";
-import "../../../third_party/iron/IRMatic.sol";
-import "../../../third_party/iron/IronPriceOracle.sol";
-import "../../../third_party/IWmatic.sol";
+import "../../../third_party/scream/PriceOracle.sol";
 import "../../interface/IScreamFoldStrategy.sol";
 import "../../interface/IScreamFoldStrategy.sol";
 
@@ -43,9 +41,7 @@ abstract contract ScreamFoldStrategyBase is FoldingBase, IScreamFoldStrategy {
   /// @dev Placeholder, for non full buyback need to implement liquidation
   uint256 private constant _BUY_BACK_RATIO = 10000;
   /// @dev SCREAM token address for reward price determination
-  address public constant SCREAM_R_TOKEN = 0xe0654C8e6fd4D733349ac7E09f6f23DA256bF475; //todo not needed???
-  address public constant W_MATIC = 0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270;
-  address public constant R_ETHER = 0xCa0F37f73174a28a64552D426590d3eD601ecCa1;
+  address public constant SCREAM_R_TOKEN = 0xe0654C8e6fd4D733349ac7E09f6f23DA256bF475;
 
   /// @notice scToken address
   address public override scToken;
@@ -76,13 +72,8 @@ abstract contract ScreamFoldStrategyBase is FoldingBase, IScreamFoldStrategy {
     require(_screamController != address(0), "IFS: Zero address ironController");
     scToken = _scToken;
     screamController = _screamController;
-
-    if (_isMatic()) {
-      require(_underlyingToken == W_MATIC, "IFS: Only wmatic allowed");
-    } else {
-      address _lpt = CompleteRToken(scToken).underlying();
-      require(_lpt == _underlyingToken, "IFS: Wrong underlying");
-    }
+    address _lpt = CompleteRToken(scToken).underlying();
+    require(_lpt == _underlyingToken, "IFS: Wrong underlying");
   }
 
   /////////////////////////////////////////////
@@ -95,7 +86,6 @@ abstract contract ScreamFoldStrategyBase is FoldingBase, IScreamFoldStrategy {
   function readyToClaim() external view override returns (uint256[] memory) {
     uint256[] memory rewards = new uint256[](1);
     rewards[0] = IScreamController(screamController).compAccrued(address(this));
-    console.log("readyToClaim: %s", rewards[0]);
     return rewards;
   }
 
@@ -126,44 +116,26 @@ abstract contract ScreamFoldStrategyBase is FoldingBase, IScreamFoldStrategy {
   function _isFoldingProfitable() internal view override returns (bool) {
     // compare values per block per 1$
 //    return rewardsRateNormalised() > foldCostRatePerToken();
-    return true; //todo fix
+    return true;
   }
 
   /// @dev Claim distribution rewards
   function _claimReward() internal override {
     address[] memory markets = new address[](1);
     markets[0] = scToken;
-    console.log("scToken %s", scToken);
-    console.log("screamController %s", screamController);
-    console.log("_rewardTokens[0] %s", _rewardTokens[0]);
-    uint256 rewardBal1 = IERC20(_rewardTokens[0]).balanceOf(address(this));
-    console.log("rewardBal1 %s", rewardBal1);
-
-
     IScreamController(screamController).claimComp(address(this), markets);
-
-    uint256 rewardBal2 = IERC20(_rewardTokens[0]).balanceOf(address(this));
-    console.log("rewardBal2 %s", rewardBal2);
   }
 
   function _supply(uint256 amount) internal override updateSupplyInTheEnd {
     amount = Math.min(IERC20(_underlyingToken).balanceOf(address(this)), amount);
-    if (_isMatic()) {
-      wmaticWithdraw(amount);
-      IRMatic(scToken).mint{value : amount}();
-    } else {
-      IERC20(_underlyingToken).safeApprove(scToken, 0);
-      IERC20(_underlyingToken).safeApprove(scToken, amount);
-      require(CompleteRToken(scToken).mint(amount) == 0, "IFS: Supplying failed");
-    }
+    IERC20(_underlyingToken).safeApprove(scToken, 0);
+    IERC20(_underlyingToken).safeApprove(scToken, amount);
+    require(CompleteRToken(scToken).mint(amount) == 0, "IFS: Supplying failed");
   }
 
   function _borrow(uint256 amountUnderlying) internal override updateSupplyInTheEnd {
     // Borrow, check the balance for this contract's address
     require(CompleteRToken(scToken).borrow(amountUnderlying) == 0, "IFS: Borrow failed");
-    if (_isMatic()) {
-      IWmatic(W_MATIC).deposit{value : address(this).balance}();
-    }
   }
 
   function _redeemUnderlying(uint256 amountUnderlying) internal override updateSupplyInTheEnd {
@@ -175,15 +147,12 @@ abstract contract ScreamFoldStrategyBase is FoldingBase, IScreamFoldStrategy {
         redeemCode = code;
       } catch{}
       if (redeemCode != 0) {
-        // iron has verification function that can ruin tx with underlying, in this case redeem rToken will work
+        // Scream has verification function that can ruin tx with underlying, in this case redeem scToken will work
         (,,, uint256 exchangeRate) = CompleteRToken(scToken).getAccountSnapshot(address(this));
-        uint256 rTokenRedeem = amountUnderlying * 1e18 / exchangeRate;
-        if (rTokenRedeem > 0) {
-          _redeemLoanToken(rTokenRedeem);
+        uint256 scTokenRedeem = amountUnderlying * 10 ** (18 - 8 + underlyingDecimals()) / exchangeRate;
+        if (scTokenRedeem > 0) {
+          _redeemLoanToken(scTokenRedeem);
         }
-      }
-      if (_isMatic()) {
-        IWmatic(W_MATIC).deposit{value : address(this).balance}();
       }
     }
   }
@@ -196,14 +165,9 @@ abstract contract ScreamFoldStrategyBase is FoldingBase, IScreamFoldStrategy {
 
   function _repay(uint256 amountUnderlying) internal override updateSupplyInTheEnd {
     if (amountUnderlying != 0) {
-      if (_isMatic()) {
-        wmaticWithdraw(amountUnderlying);
-        IRMatic(scToken).repayBorrow{value : amountUnderlying}();
-      } else {
-        IERC20(_underlyingToken).safeApprove(scToken, 0);
-        IERC20(_underlyingToken).safeApprove(scToken, amountUnderlying);
-        require(CompleteRToken(scToken).repayBorrow(amountUnderlying) == 0, "IFS: Repay failed");
-      }
+      IERC20(_underlyingToken).safeApprove(scToken, 0);
+      IERC20(_underlyingToken).safeApprove(scToken, amountUnderlying);
+      require(CompleteRToken(scToken).repayBorrow(amountUnderlying) == 0, "IFS: Repay failed");
     }
   }
 
@@ -221,9 +185,9 @@ abstract contract ScreamFoldStrategyBase is FoldingBase, IScreamFoldStrategy {
 
     // we have a little amount of supply after full exit
     // better to redeem rToken amount for avoid rounding issues
-    (,uint256 rTokenBalance,,) = CompleteRToken(scToken).getAccountSnapshot(address(this));
-    if (rTokenBalance > 0) {
-      _redeemLoanToken(rTokenBalance);
+    (,uint256 scTokenBalance,,) = CompleteRToken(scToken).getAccountSnapshot(address(this));
+    if (scTokenBalance > 0) {
+      _redeemLoanToken(scTokenBalance);
     }
   }
 
@@ -244,9 +208,9 @@ abstract contract ScreamFoldStrategyBase is FoldingBase, IScreamFoldStrategy {
     CompleteRToken rt = CompleteRToken(scToken);
 
     // get reward per token for both - suppliers and borrowers
-    uint256 rewardSpeed = IScreamController(screamController).rewardSpeeds(scToken);
+    uint256 rewardSpeed = IScreamController(screamController).compSpeeds(scToken);
     // using internal Iron Oracle the safest way
-    uint256 rewardTokenPrice = rTokenUnderlyingPrice(SCREAM_R_TOKEN);
+    uint256 rewardTokenPrice = scTokenUnderlyingPrice(SCREAM_R_TOKEN);
     // normalize reward speed to USD price
     uint256 rewardSpeedUsd = rewardSpeed * rewardTokenPrice / 1e18;
 
@@ -259,11 +223,11 @@ abstract contract ScreamFoldStrategyBase is FoldingBase, IScreamFoldStrategy {
       return 0;
     }
 
-    // exchange rate between rToken and underlyingToken
-    uint256 rTokenExchangeRate = rt.exchangeRateStored() * (10 ** decimals()) / (10 ** underlyingDecimals());
+    // exchange rate between scToken and underlyingToken
+    uint256 scTokenExchangeRate = rt.exchangeRateStored() * (10 ** decimals()) / (10 ** underlyingDecimals());
 
     // amount of reward tokens per block for 1 supplied underlyingToken
-    uint256 rewardSpeedUsdPerSuppliedToken = rewardSpeedUsd * 1e18 / rTokenExchangeRate * 1e18 / totalSupply / 2;
+    uint256 rewardSpeedUsdPerSuppliedToken = rewardSpeedUsd * 1e18 / scTokenExchangeRate * 1e18 / totalSupply / 2;
     // amount of reward tokens per block for 1 borrowed underlyingToken
     uint256 rewardSpeedUsdPerBorrowedToken = rewardSpeedUsd * 1e18 / totalBorrows / 2;
 
@@ -279,31 +243,25 @@ abstract contract ScreamFoldStrategyBase is FoldingBase, IScreamFoldStrategy {
       return 1;
     }
     uint256 foldRateCost = rt.borrowRatePerBlock() - rt.supplyRatePerBlock();
-    uint256 _rTokenPrice = rTokenUnderlyingPrice(scToken);
+    uint256 _scTokenPrice = scTokenUnderlyingPrice(scToken);
 
     // let's calculate profit for 1 token
-    return foldRateCost * _rTokenPrice / 1e18;
+    return foldRateCost * _scTokenPrice / 1e18;
   }
 
-  /// @dev Return rToken price from Iron Oracle solution. Can be used on-chain safely
-  function rTokenUnderlyingPrice(address _rToken) public view returns (uint256){
-    uint256 _rTokenPrice = IronPriceOracle(
-      IScreamController(screamController).oracle()
-    ).getUnderlyingPrice(_rToken);
+  /// @dev Return scToken price from Scream Oracle solution. Can be used on-chain safely
+  function scTokenUnderlyingPrice(address _scToken) public view returns (uint256){
+    address oracleAddr = IScreamController(screamController).oracle();
+    console.log("oracleAddr %s", oracleAddr);
+    console.log("_scToken %s", _scToken);
+    uint256 _scTokenPrice = PriceOracle(oracleAddr).getUnderlyingPrice(_scToken);
+    console.log("_scTokenPrice %s", _scTokenPrice);
+
     // normalize token price to 1e18
     if (underlyingDecimals() < 18) {
-      _rTokenPrice = _rTokenPrice / (10 ** (18 - underlyingDecimals()));
+      _scTokenPrice = _scTokenPrice / (10 ** (18 - underlyingDecimals()));
     }
-    return _rTokenPrice;
-  }
-
-  function wmaticWithdraw(uint256 amount) private {
-    require(IERC20(W_MATIC).balanceOf(address(this)) >= amount, "IFS: Not enough wmatic");
-    IWmatic(W_MATIC).withdraw(amount);
-  }
-
-  function _isMatic() internal view returns (bool) {
-    return scToken == R_ETHER;
+    return _scTokenPrice;
   }
 
 }
