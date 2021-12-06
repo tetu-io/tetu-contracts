1900583ad7bc9354cec536abe782d7f41e2089a0
olegnat <natsevsky@gmail.com>
2021-12-06 17:07:10 +0100

    - add Fantom addresses

--- scripts/addresses/FtmAddresses.ts
+++ scripts/addresses/FtmAddresses.ts
@@ -17,6 +17,16 @@ export class FtmAddresses {
   public static BOO_TOKEN = "0x841FAD6EAe12c286d1Fd18d1d525DFfA75C7EFFE".toLowerCase();
   public static GEIST_TOKEN = "0xd8321AA83Fb0a4ECd6348D4577431310A6E0814d".toLowerCase();
   public static SCREAM_TOKEN = "0xe0654C8e6fd4D733349ac7E09f6f23DA256bF475".toLowerCase();
+  public static YFI_TOKEN = "0x29b0Da86e484E1C0029B56e817912d778aC0EC69".toLowerCase();
+  public static CRV_TOKEN = "0x1E4F97b9f9F913c46F1632781732927B9019C68b".toLowerCase();
+  public static FUSD_TOKEN = "0xAd84341756Bf337f5a0164515b1f6F993D194E1f".toLowerCase();
+  public static LINK_TOKEN = "0xb3654dc3D10Ea7645f8319668E8F54d2574FBdC8".toLowerCase();
+  public static DOLA_TOKEN = "0x3129662808bEC728a27Ab6a6b9AFd3cBacA8A43c".toLowerCase();
+  public static MIM_TOKEN = "0x82f0B8B456c1A451378467398982d4834b6829c1".toLowerCase();
+  public static BIFI_TOKEN = "0xd6070ae98b8069de6B494332d1A1a81B6179D960".toLowerCase();
+  public static TUSD_TOKEN = "0x9879aBDea01a879644185341F7aF7d8343556B7a".toLowerCase();
+  public static FBTC_TOKEN = "0xe1146b9AC456fCbB60644c36Fd3F868A9072fc6E".toLowerCase();
+  public static FETH_TOKEN = "0x658b0c7613e890EE50B8C4BC6A3f41ef411208aD".toLowerCase();
 
   // SpookySwap
   public static SPOOKY_SWAP_FACTORY = "0x152eE697f2E276fA89E96742e9bB9aB1F2E61bE3".toLowerCase();
@@ -24,7 +34,7 @@ export class FtmAddresses {
   public static SPOOKY_MASTERCHEF = "0x2b2929E785374c651a81A63878Ab22742656DcDd".toLowerCase();
 
   // Scream
-  public static SCREAM_CONTROLLER = "0x37517C5D880c5c282437a3Da4d627B4457C10BEB".toLowerCase();
+  public static SCREAM_CONTROLLER = "0x260E596DAbE3AFc463e75B6CC05d8c46aCAcFB09".toLowerCase();
   // TetuSwap
   public static TETU_SWAP_FACTORY = "".toLowerCase();
   public static TETU_SWAP_ROUTER = "".toLowerCase();
