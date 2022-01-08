1babc2a2e7ee8b01306d2aafdb192ae2fa344622
iamkosyak <iamkosyak@gmai.com>
2022-01-08 23:37:53 +0300

    fix ContractReaderTest.ts add tests to ContractUtilsTest.ts

--- test/tools/ContractUtilsTest.ts
+++ test/tools/ContractUtilsTest.ts
@@ -5,6 +5,8 @@ import {ethers} from "hardhat";
 import {TimeUtils} from "../TimeUtils";
 import {ContractUtils} from "../../typechain";
 import {DeployerUtils} from "../../scripts/deploy/DeployerUtils";
+import {TokenUtils} from "../TokenUtils";
+import {utils} from "ethers";
 
 const {expect} = chai;
 chai.use(chaiAsPromised);
@@ -12,17 +14,26 @@ chai.use(chaiAsPromised);
 describe("Contract utils tests", function () {
   let snapshot: string;
   let signer: SignerWithAddress;
-  let utils: ContractUtils;
+  let util: ContractUtils;
+  let addressWithUsdc: ContractUtils;
+  let addressWithoutUsdc: ContractUtils;
 
   const ercTokens: string[] = [];
+  const addresses: string[] = [];
 
   before(async function () {
     this.timeout(1200000);
     snapshot = await TimeUtils.snapshot();
     signer = (await ethers.getSigners())[0];
-    utils = await DeployerUtils.deployContract(signer, "ContractUtils") as ContractUtils;
+    util = await DeployerUtils.deployContract(signer, "ContractUtils") as ContractUtils;
+    addressWithUsdc = await DeployerUtils.deployContract(signer, "ContractUtils") as ContractUtils;
+    addressWithoutUsdc = await DeployerUtils.deployContract(signer, "ContractUtils") as ContractUtils;
+    addresses.push(addressWithUsdc.address)
+    addresses.push(addressWithoutUsdc.address)
     ercTokens.push(await DeployerUtils.getUSDCAddress())
     ercTokens.push(await DeployerUtils.getNetworkTokenAddress())
+    await TokenUtils.getToken(ercTokens[0], signer.address, utils.parseUnits('1000', 6));
+    await TokenUtils.transfer(ercTokens[0], signer, addresses[0], "1000000000");
   });
 
   after(async function () {
@@ -31,23 +42,30 @@ describe("Contract utils tests", function () {
 
 
   it("symbols", async () => {
-    expect((await utils.erc20Symbols(ercTokens))[0]).is.eq('USDC');
+    expect((await util.erc20Symbols(ercTokens))[0]).is.eq('USDC');
   });
 
   it("names", async () => {
-    expect((await utils.erc20Names(ercTokens))[0]).is.contain('USD Coin');
+    expect((await util.erc20Names(ercTokens))[0]).is.contain('USD Coin');
   });
 
   it("decimals", async () => {
-    expect((await utils.erc20Decimals(ercTokens))[0]).is.eq(6);
+    expect((await util.erc20Decimals(ercTokens))[0]).is.eq(6);
   });
 
   it("balances", async () => {
-    expect((await utils.erc20Balances(ercTokens, signer.address))[0]).is.eq(0);
+    expect((await util.erc20Balances(ercTokens, signer.address))[0]).is.eq(0);
   });
 
   it("supply", async () => {
-    expect((await utils.erc20TotalSupply(ercTokens))[0]).is.not.eq(0);
+    expect((await util.erc20TotalSupply(ercTokens))[0]).is.not.eq(0);
   });
 
+  it("balances_for_addresses_not_0", async() =>{
+    expect((await util.erc20BalancesForAddresses(ercTokens[0], addresses))[0]).is.not.eq(0);
+  });
+
+  it("balances_for_addresses_is_0", async() =>{
+    expect((await util.erc20BalancesForAddresses(ercTokens[0], addresses))[1]).is.eq(0);
+  });
 });
