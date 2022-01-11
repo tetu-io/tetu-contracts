606835e36f8ba70bb2589185db0eafdd736fbb46
iamkosyak <iamkosyak@gmail.com>
2022-01-11 17:41:02 +0300

    Update ArrayLibTest

--- contracts/test/ArrayLibTest.sol
+++ contracts/test/ArrayLibTest.sol
@@ -1,3 +1,5 @@
+// SPDX-License-Identifier: ISC
+
 pragma solidity 0.8.4;
 import "../base/ArrayLib.sol";
 
@@ -8,111 +10,153 @@ contract ArrayLibTest {
   using ArrayLib for address[];
   using ArrayLib for uint256[];
 
-  function containsUint(uint256[] calldata _uint, uint256 value) public returns (bool) {
+  function containsUint(
+    uint256[] calldata _uint,
+    uint256 value
+  ) public returns (bool) {
     delete uintArray;
-    for (uint i=0; i<_uint.length; i++){
+    for (uint i=0; i<_uint.length; i++) {
       uintArray.push(_uint[i]);
     }
     bool b = uintArray.contains(value);
     return b;
   }
 
-  function containsAddress(address[] calldata _address, address value) public returns (bool) {
+  function containsAddress(
+    address[] calldata _address,
+    address value
+  ) public returns (bool) {
     delete addressArray;
-    for (uint i=0; i<_address.length; i++){
+    for (uint i=0; i<_address.length; i++) {
       addressArray.push(_address[i]);
     }
     bool b = addressArray.contains(value);
     return b;
   }
 
-  function addUniqueAddress(address[] calldata _address, address value) public returns(address[] memory) {
-    delete addressArray;
-    for (uint i=0; i<_address.length; i++){
-    addressArray.push(_address[i]);
-    }
-    addressArray.addUnique(value);
-    return addressArray;
-  }
-
-  function addUniqueUint(uint256[] calldata _uint, uint256 value) public returns(uint256[] memory){
+  function addUniqueUint(
+    uint256[] calldata _uint,
+    uint256 value
+  ) public returns(uint256[] memory) {
     delete uintArray;
-    for (uint i=0; i<_uint.length; i++){
+    for (uint i=0; i<_uint.length; i++) {
       uintArray.push(_uint[i]);
     }
     uintArray.addUnique(value);
     return uintArray;
   }
 
-  function addUniqueArrayAddress(address[] calldata _address, address[] memory value) public returns(address[] memory){
+  function addUniqueAddress(
+    address[] calldata _address,
+    address value
+  ) public returns(address[] memory) {
     delete addressArray;
-    for (uint i=0; i<_address.length; i++){
+    for (uint i=0; i<_address.length; i++) {
       addressArray.push(_address[i]);
     }
-   addressArray.addUniqueArray(value);
-   return addressArray;
+    addressArray.addUnique(value);
+    return addressArray;
   }
 
-  function addUniqueArrayUint(uint256[] calldata _uint, uint256[] memory value) public returns(uint256[] memory){
+  function addUniqueArrayUint(
+    uint256[] calldata _uint,
+    uint256[] memory value
+  ) public returns(uint256[] memory) {
     delete uintArray;
-    for (uint i=0; i<_uint.length; i++){
+    for (uint i=0; i<_uint.length; i++) {
       uintArray.push(_uint[i]);
     }
     uintArray.addUniqueArray(value);
     return uintArray;
   }
 
-  function removeByIndexAddress(address[] calldata _address, uint256 value) public returns(address[] memory){
+  function addUniqueArrayAddress(
+    address[] calldata _address,
+    address[] memory value
+  ) public returns(address[] memory){
     delete addressArray;
-    for (uint i=0; i<_address.length; i++){
+    for (uint i=0; i<_address.length; i++) {
       addressArray.push(_address[i]);
     }
-    addressArray.removeByIndex(value, true);
+    addressArray.addUniqueArray(value);
     return addressArray;
   }
 
-  function removeByIndexUint(uint256[] calldata _uint, uint256 value) public returns(uint256[] memory){
+  function removeByIndexUint(
+    uint256[] calldata _uint,
+    uint256 value,
+    bool _bool
+  ) public returns(uint256[] memory) {
     delete uintArray;
-    for (uint i=0; i<_uint.length; i++){
+    for (uint i=0; i<_uint.length; i++) {
       uintArray.push(_uint[i]);
     }
-    uintArray.removeByIndex(value, true);
+    uintArray.removeByIndex(value, _bool);
     return uintArray;
   }
 
-  function findAndRemoveAddress(address[] calldata _address, address value) public returns(address[] memory){
+  function removeByIndexAddress(
+    address[] calldata _address,
+    uint256 value,
+    bool _bool
+  ) public returns(address[] memory) {
     delete addressArray;
-    for (uint i=0; i<_address.length; i++){
+    for (uint i=0; i<_address.length; i++) {
       addressArray.push(_address[i]);
     }
-    addressArray.findAndRemove(value, true);
+    addressArray.removeByIndex(value, _bool);
     return addressArray;
   }
 
-  function findAndRemoveUint(uint256[] calldata _uint, uint256 value) public returns(uint256[] memory){
+  function findAndRemoveUint(
+    uint256[] calldata _uint,
+    uint256 value,
+    bool _bool
+  ) public returns(uint256[] memory) {
     delete uintArray;
-    for (uint i=0; i<_uint.length; i++){
+    for (uint i=0; i<_uint.length; i++) {
       uintArray.push(_uint[i]);
     }
-    uintArray.findAndRemove(value, true);
+    uintArray.findAndRemove(value, _bool);
     return uintArray;
   }
 
-  function findAndRemoveArrayAddress(address[] calldata _address, address[] memory value) public returns(address[] memory){
+  function findAndRemoveAddress(
+    address[] calldata _address,
+    address value,
+    bool _bool
+  ) public returns(address[] memory) {
     delete addressArray;
-    for (uint i=0; i<_address.length; i++){
+    for (uint i=0; i<_address.length; i++) {
       addressArray.push(_address[i]);
     }
-    addressArray.findAndRemoveArray(value, true);
+    addressArray.findAndRemove(value, _bool);
     return addressArray;
   }
 
-  function findAndRemoveArrayUint256(uint256[] calldata _uint, uint256[] memory value) public returns(uint256[] memory){
+  function findAndRemoveArrayUint(
+    uint256[] calldata _uint,
+    uint256[] memory value,
+    bool _bool
+  ) public returns(uint256[] memory) {
     delete uintArray;
-    for (uint i=0; i<_uint.length; i++){
+    for (uint i=0; i<_uint.length; i++) {
       uintArray.push(_uint[i]);
     }
-    uintArray.findAndRemoveArray(value, true);
+    uintArray.findAndRemoveArray(value, _bool);
     return uintArray;
+  }
+
+  function findAndRemoveArrayAddress(
+    address[] calldata _address,
+    address[] memory value,
+    bool _bool
+  ) public returns(address[] memory) {
+    delete addressArray;
+    for (uint i=0; i<_address.length; i++) {
+      addressArray.push(_address[i]);
+    }
+    addressArray.findAndRemoveArray(value, _bool);
+    return addressArray;
   }
 }
