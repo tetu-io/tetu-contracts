c6ac10c1109c56d3294758eb268fbcaab068d6ae
iamkosyak <iamkosyak@gmail.com>
2022-01-17 13:18:34 +0300

    Fix hardhat.config.ts changes

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -17,41 +17,41 @@ import "@tenderly/hardhat-tenderly"
 dotEnvConfig();
 // tslint:disable-next-line:no-var-requires
 const argv = require('yargs/yargs')()
-    .env('TETU')
-    .options({
-      hardhatChainId: {
-        type: "number",
-        default: 137
-      },
-      maticRpcUrl: {
-        type: "string",
-      },
-      ftmRpcUrl: {
-        type: "string",
-      },
-      ethRpcUrl: {
-        type: "string",
-        default: ''
-      },
-      infuraKey: {
-        type: "string",
-      },
-      networkScanKey: {
-        type: "string",
-      },
-      privateKey: {
-        type: "string",
-        default: "85bb5fa78d5c4ed1fde856e9d0d1fe19973d7a79ce9ed6c0358ee06a4550504e" // random account
-      },
-      maticForkBlock: {
-        type: "number",
-        default: 23238072
-      },
-      ftmForkBlock: {
-        type: "number",
-        default: 27468274
-      },
-    }).argv;
+  .env('TETU')
+  .options({
+    hardhatChainId: {
+      type: "number",
+      default: 137
+    },
+    maticRpcUrl: {
+      type: "string",
+    },
+    ftmRpcUrl: {
+      type: "string",
+    },
+    ethRpcUrl: {
+      type: "string",
+      default: ''
+    },
+    infuraKey: {
+      type: "string",
+    },
+    networkScanKey: {
+      type: "string",
+    },
+    privateKey: {
+      type: "string",
+      default: "85bb5fa78d5c4ed1fde856e9d0d1fe19973d7a79ce9ed6c0358ee06a4550504e" // random account
+    },
+    maticForkBlock: {
+      type: "number",
+      default: 23238072
+    },
+    ftmForkBlock: {
+      type: "number",
+      default: 27468274
+    },
+  }).argv;
 
 
 export default {
@@ -62,17 +62,17 @@ export default {
       chainId: argv.hardhatChainId,
       timeout: 99999 * 2,
       gas: argv.hardhatChainId === 137 ? 19_000_000 :
-          argv.hardhatChainId === 250 ? 11_000_000 :
-              9_000_000,
+        argv.hardhatChainId === 250 ? 11_000_000 :
+          9_000_000,
       forking: {
         url:
-            argv.hardhatChainId === 137 ? argv.maticRpcUrl :
-                argv.hardhatChainId === 250 ? argv.ftmRpcUrl :
-                    undefined,
+          argv.hardhatChainId === 137 ? argv.maticRpcUrl :
+            argv.hardhatChainId === 250 ? argv.ftmRpcUrl :
+              undefined,
         blockNumber:
-            argv.hardhatChainId === 137 ? argv.maticForkBlock !== 0 ? argv.maticForkBlock : undefined :
-                argv.hardhatChainId === 250 ? argv.ftmForkBlock !== 0 ? argv.ftmForkBlock : undefined :
-                    undefined
+          argv.hardhatChainId === 137 ? argv.maticForkBlock !== 0 ? argv.maticForkBlock : undefined :
+            argv.hardhatChainId === 250 ? argv.ftmForkBlock !== 0 ? argv.ftmForkBlock : undefined :
+              undefined
       },
       accounts: {
         mnemonic: "test test test test test test test test test test test junk",
