48acd855ea02e05db7feeb2f21f5c3c788a01f44
groninge <groninge@hotmail.com>
2022-01-21 19:43:28 +0100

    add eslint prettier plugin

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,6 +17,10 @@ module.exports = {
         "es6": true,
         "node": true
     },
+    "extends": [
+        "prettier",
+        "prettier/@typescript-eslint"
+    ],
     "parser": "@typescript-eslint/parser",
     "parserOptions": {
         "project": "tsconfig.json",
