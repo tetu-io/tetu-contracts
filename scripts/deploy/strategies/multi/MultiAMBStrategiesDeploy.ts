173b8dc4af702d2d393964434c21ddddb327f0fb
Viacheslav Bogdanov <viacheslav.v.bogdanov@gmail.com>
2022-03-15 15:16:02 +0500

    MB Deploy Script

--- scripts/deploy/strategies/multi/MultiAMBStrategiesDeploy.ts
+++ scripts/deploy/strategies/multi/MultiAMBStrategiesDeploy.ts
@@ -133,8 +133,8 @@ async function main() {
 }
 
 main()
-.then(() => process.exit(0))
-.catch(error => {
-  console.error(error);
-  process.exit(1);
+  .then(() => process.exit(0))
+  .catch(error => {
+    console.error(error);
+    process.exit(1);
 });
