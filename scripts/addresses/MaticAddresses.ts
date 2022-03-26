bcea9e4350e2ee6a16bbe86eb56ff820cb17e754
belbix <7453635@gmail.com>
2022-03-26 18:10:02 +0300

    uma platform

--- scripts/addresses/MaticAddresses.ts
+++ scripts/addresses/MaticAddresses.ts
@@ -66,6 +66,7 @@ export class MaticAddresses {
   public static NSHARE_TOKEN = '0x948D0a28b600BDBd77AF4ea30E6F338167034181'.toLowerCase();
   public static NBOND_NOKEN = '0xfc4a30f328E946ef3E727BD294a93e84c2e43c24'.toLowerCase();
   public static UNT_NOKEN = '0xc46DB78Be28B5F2461097ed9e3Fcc92E9FF8676d'.toLowerCase();
+  public static UMA_NOKEN = '0x3066818837c5e6eD6601bd5a91B0762877A6B731'.toLowerCase();
 
   // public static dQUICK_TOKEN = "".toLowerCase();
 
--- scripts/deploy/swap/VaultForLP.ts
+++ scripts/deploy/swap/VaultForLP.ts
@@ -52,7 +52,7 @@ async function main() {
   // for (let i = 0; i < length; i++) {
   for (let i = 0; i < 1; i++) {
     //   const pair = await factory.allPairs(i);
-    const pair = '0xe8F56B590ea274d5D451F786c3270af764F1b793';
+    const pair = '0xAbcA7538233cbE69709C004c52DC37e61c03796B';
     const pairCtr = await DeployerUtils.connectInterface(signer, 'TetuSwapPair', pair) as TetuSwapPair;
     const token0 = await pairCtr.token0();
     const token1 = await pairCtr.token1();
