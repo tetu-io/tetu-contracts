bf527620a496a7354b5b67f4cdf931c720cdc5ed
dvpublic <dvpublic0@gmail.com>
2022-05-03 09:29:45 +0700

    Declare ITricryptoPools.exchange_underlying in same way as it's declared in TricryptoPool => fix Tricrypto-tests

--- contracts/third_party/curve/ITricryptoPool.sol
+++ contracts/third_party/curve/ITricryptoPool.sol
@@ -2,10 +2,10 @@
 
 pragma solidity 0.8.4;
 
+/// see https://curve.readthedocs.io/exchange-pools.html
 interface ITricryptoPool {
 
   function add_liquidity(uint256[5] calldata amounts, uint256 min_mint_amount) external;
 
-  function exchange_underlying(int128 i, int128 j, uint256 dx, uint256 min_dy, address receiver) external;
-
+  function exchange_underlying(uint256 i, uint256 j, uint256 dx, uint256 min_dy, address receiver) external;
 }
