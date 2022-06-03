28bb18fe9c0a3fb891581be7048a8e3a666af91e
Viacheslav Bogdanov <viacheslav.v.bogdanov@gmail.com>
2022-06-03 13:27:32 +0500

    deploy contract utils

--- scripts/deploy/tools/DeployContractUtils.ts
+++ scripts/deploy/tools/DeployContractUtils.ts
@@ -2,6 +2,10 @@ import {DeployerUtils} from "../DeployerUtils";
 import {ethers} from "hardhat";
 import {ContractUtils} from "../../../typechain";
 
+// latest deployed by bogdoslav
+// Deploy tx: 0x624d00e2112edc3026701a3251ae3e890b8add479cc2d0fd46cdf1d9d154f1e1
+// 2022-06-01 17:46:59.035 >>>ContractUtils deployed 0xd933B5943B82806C638df8c0C88dC0930Dd13bE4 gas used: 1422306 10.4 sec
+
 
 async function main() {
   const signer = (await ethers.getSigners())[0];
