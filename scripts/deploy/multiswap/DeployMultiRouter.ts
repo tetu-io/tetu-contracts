03ea5eaa0b79c10ffa8858fbe7953dbc38e30ca6
Viacheslav Bogdanov <viacheslav.v.bogdanov@gmail.com>
2022-06-20 15:27:26 +0700

    deploy multiswap2

--- scripts/deploy/multiswap/DeployMultiRouter.ts
+++ scripts/deploy/multiswap/DeployMultiRouter.ts
@@ -3,22 +3,24 @@ import {DeployerUtils} from "../DeployerUtils";
 import {MultiSwap2} from "../../../typechain";
 import {MaticAddresses} from "../../addresses/MaticAddresses";
 
-// Latest:  (with SOR support)
+// Latest: 0x8b56D66cCbc34DAd3e8ae214b9aA2BF752dF1041 (with SOR support)
 // Prev  : 0x6dB6CeA8BB997525164a8960d74143685b0a00F7
 
 async function main() {
   const signer = (await ethers.getSigners())[0];
   const core = await DeployerUtils.getCoreAddresses();
   const networkToken = await DeployerUtils.getNetworkTokenAddress();
-
-  const contract = await DeployerUtils.deployContract(
-      signer, "MultiSwap2",
+  const args = [
       core.controller,
       networkToken,
-      MaticAddresses.BALANCER_VAULT) as MultiSwap2;
+      MaticAddresses.BALANCER_VAULT
+  ]
+  const contract = await DeployerUtils.deployContract(
+      signer, "MultiSwap2",
+      ...args) as MultiSwap2;
 
   await DeployerUtils.wait(5);
-  await DeployerUtils.verify(contract.address);
+  await DeployerUtils.verifyWithArgs(contract.address, args);
 }
 
 main()
