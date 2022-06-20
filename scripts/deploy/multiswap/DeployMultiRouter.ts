4dfc3b0636cba07e81d6e00aac51ef6b6be41a4e
Viacheslav Bogdanov <viacheslav.v.bogdanov@gmail.com>
2022-06-20 13:16:41 +0700

    pre-deploy multiswap2

--- scripts/deploy/multiswap/DeployMultiRouter.ts
+++ scripts/deploy/multiswap/DeployMultiRouter.ts
@@ -1,18 +1,24 @@
 import {ethers} from "hardhat";
 import {DeployerUtils} from "../DeployerUtils";
 import {MultiSwap2} from "../../../typechain";
+import {MaticAddresses} from "../../addresses/MaticAddresses";
 
-// Latest: 0x6dB6CeA8BB997525164a8960d74143685b0a00F7 (with loadPairReserves)
-// Prev  : 0xfD965c24c9b9B33802d6064549CC4d0b9A604786
+// Latest:  (with SOR support)
+// Prev  : 0x6dB6CeA8BB997525164a8960d74143685b0a00F7
 
 async function main() {
   const signer = (await ethers.getSigners())[0];
   const core = await DeployerUtils.getCoreAddresses();
+  const networkToken = await DeployerUtils.getNetworkTokenAddress();
 
-  const contract = await DeployerUtils.deployContract(signer, "MultiSwap2") as MultiSwap2;
+  const contract = await DeployerUtils.deployContract(
+      signer, "MultiSwap2",
+      core.controller,
+      networkToken,
+      MaticAddresses.BALANCER_VAULT) as MultiSwap2;
 
   await DeployerUtils.wait(5);
-  await DeployerUtils.verify(contract.address); // TODO why address is wrong? Fix address and verification
+  await DeployerUtils.verify(contract.address);
 }
 
 main()
