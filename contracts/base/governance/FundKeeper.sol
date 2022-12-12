f3cc87df2f83d794dcf9d653268a1a6616184619
belbix <7453635@gmail.com>
2022-12-12 18:47:28 +0300

    fund keeper improvements

--- contracts/base/governance/FundKeeper.sol
+++ contracts/base/governance/FundKeeper.sol
@@ -15,6 +15,7 @@ pragma solidity 0.8.4;
 
 import "./ControllableV2.sol";
 import "../interface/IFundKeeper.sol";
+import "../interface/ISmartVault.sol";
 import "../../openzeppelin/IERC20.sol";
 import "../../openzeppelin/SafeERC20.sol";
 
@@ -24,8 +25,23 @@ import "../../openzeppelin/SafeERC20.sol";
 contract FundKeeper is ControllableV2, IFundKeeper {
   using SafeERC20 for IERC20;
 
+  // -------- CONSTANTS ------------
+
+  uint public constant DEPOSIT_TIME_LOCK = 18 hours;
+
+  // -------- VARIABLES ------------
+
+  mapping(address => uint) public depositTimeLocks;
+
+  // -------- EVENTS ------------
+
   /// @notice Governance moved token to Controller
   event TokenWithdrawn(address indexed token, uint256 amount);
+  event DepositToVaultAnnounced(address vault);
+  event DepositedToVault(address vault, uint amount);
+  event WithdrewFromVault(address vault, uint numberOfShares);
+
+  // -------- INIT ------------
 
   /// @notice Initialize contract after setup it as proxy implementation
   /// @dev Use it only once after first logic setup
@@ -34,11 +50,21 @@ contract FundKeeper is ControllableV2, IFundKeeper {
     ControllableV2.initializeControllable(_controller);
   }
 
+  // -------- RESTRICTIONS ------------
+
   /// @dev Allow operation only for Controller
   modifier onlyController() {
     require(_controller() == msg.sender, "Not controller");
     _;
   }
+
+  /// @dev Allow operation only for governance
+  modifier onlyGov() {
+    require(_isGovernance(msg.sender), "Not gov");
+    _;
+  }
+
+  // -------- MAIN LOGIC ------------
 
   /// @notice Move tokens to controller where money will be protected with time lock
   /// @param _token Token address
@@ -48,6 +74,30 @@ contract FundKeeper is ControllableV2, IFundKeeper {
     require(tokenBalance >= amount, "not enough balance");
     IERC20(_token).safeTransfer(_controller(), amount);
     emit TokenWithdrawn(_token, amount);
+  }
+
+  function announceVaultDeposit(address vault) external onlyGov {
+    require(depositTimeLocks[vault] == 0, "announced");
+    depositTimeLocks[vault] = block.timestamp + DEPOSIT_TIME_LOCK;
+    emit DepositToVaultAnnounced(vault);
+  }
+
+  function depositToVault(address vault, uint amount) external onlyGov {
+    require(IController(_controller()).isValidVault(vault), "!vault");
+    require(depositTimeLocks[vault] != 0 && depositTimeLocks[vault] < block.timestamp, "time lock");
+
+    depositTimeLocks[vault] = 0;
+
+    address underlying = ISmartVault(vault).underlying();
+    IERC20(underlying).safeApprove(vault, amount);
+    ISmartVault(vault).depositAndInvest(amount);
+    emit DepositedToVault(vault, amount);
+  }
+
+  function withdrawFromVault(address vault, uint numberOfShares) external onlyGov {
+    require(IController(_controller()).isValidVault(vault), "!vault");
+    ISmartVault(vault).withdraw(numberOfShares);
+    emit WithdrewFromVault(vault, numberOfShares);
   }
 
 }
