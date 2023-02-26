b05f63f487c2cbe296635a77003d29efc3291f29
belbix <7453635@gmail.com>
2023-02-26 19:45:00 +0300

    fix tests

--- scripts/deploy/DeployContract.ts
+++ scripts/deploy/DeployContract.ts
@@ -76,7 +76,7 @@ export async function deployContract<T extends ContractFactory>(
   //   gas = 5_000_000;
   // }
   // const instance = await _factory.deploy(...args, {gasLimit: gas, gasPrice: Math.floor(+gasPrice * 1.1)});
-  const instance = await _factory.deploy(...args, {gasLimit: 9_000_000, gasPrice: Math.floor(+gasPrice * 1.1)});
+  const instance = await _factory.deploy(...args, {gasLimit: 30_000_000, gasPrice: Math.floor(+gasPrice * 1.1)});
   log.info('Deploy tx:', instance.deployTransaction.hash);
   await instance.deployed();
 
