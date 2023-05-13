a24cef43e41fb04866abae23bb81fb02e9e70f70
belbix <7453635@gmail.com>
2023-05-13 19:44:02 +0300

    deploy pawnshop

--- scripts/deploy/loan/PawnShopDeploy.ts
+++ scripts/deploy/loan/PawnShopDeploy.ts
@@ -2,7 +2,7 @@ import {DeployerUtils} from "../DeployerUtils";
 import {ethers} from "hardhat";
 import {TetuPawnShop} from "../../../typechain";
 import {parseUnits} from "ethers/lib/utils";
-import {RunHelper} from "../../utils/tools/RunHelper";
+import {MaticAddresses} from "../../addresses/MaticAddresses";
 
 
 async function main() {
@@ -10,8 +10,8 @@ async function main() {
   const gov = await DeployerUtils.getGovernance()
 
   const owner = gov;
-  const depositToken = '0x0EFc2D2D054383462F2cD72eA2526Ef7687E1016'
-  const positionDepositAmount = parseUnits('1')
+  const depositToken = MaticAddresses.TETU_TOKEN;
+  const positionDepositAmount = parseUnits('0.01')
   const feeRecipient = gov;
 
   const args = [
@@ -21,12 +21,7 @@ async function main() {
     feeRecipient,
   ];
 
-  const contract = await DeployerUtils.deployContract(signer, "TetuPawnShop", ...args) as TetuPawnShop;
-
-  // await RunHelper.runAndWait(() => contract.setPlatformFee(500));
-
-  await DeployerUtils.wait(5);
-  await DeployerUtils.verifyWithArgs(contract.address, args);
+  await DeployerUtils.deployContract(signer, "TetuPawnShop", ...args) as TetuPawnShop;
 }
 
 main()
